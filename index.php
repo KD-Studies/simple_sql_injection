65eed8c6a0ba7e2aa962032b240bdb958ba692d6
Kursat <75574588+DKursat@users.noreply.github.com>
2024-08-07 11:45:42 +0200

    Setup SQL Injection

--- index.php
+++ index.php
@@ -1,64 +1,38 @@
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>SQL Injection form error example</title>
- <meta name="description" content="Twitter Bootstrap Version2.0 form error example from w3resource.com."> <link href="http://localhost/twitter-bootstrap/twitter-bootstrap-v2/docs/assets/css/bootstrap.css" rel="stylesheet">
- </head>
- <body style="margin-top: 50px">
- <div class="container">
- <div class="row">
- <div class="span6">
- <?php
- $host="localhost";
- $username="root";
- $password="";
- $db_name="hr";
- $con=mySQL_connect("$host","$username","$password") or die("cannot connect"); 
-mySQL_select_db("$db_name") or die("cannot select DB");
-$uid = $_POST['uid'];
-$pid = $_POST['passid'];
-$SQL = "select * from user_details where userid = '$uid' and password = '$pid' ";
-$result = mySQL_query($SQL);
-if(mySQL_num_rows($result)>0)
-{echo "<h4>".
-"-- Personal Information -- ".
-$row[3]."</h4>",
-"</br>";
-while ($row=mySQL_fetch_row($result)){echo "
-<p>".
-"User ID : ".
-$row[0]."
-</p>";
-echo "<p>".
-"Password : ".
-$row[1]."</p>";
-echo "<p>".
-"First Name : ".
-$row[2]." Last Name : ".
-$row[3]."</p>";
-echo "<p>".
-"Gender : ".
-$row[4]." 
-Date of Birth :".
-$row[5]."</p>
-";echo "
-<p>".
-"Country : ".
-$row[6]." 
-User rating : ".$row[7].
-"</p>
-";
-echo "<p>
-"."Email ID : ".
-$row[8].
-"</p>
-";
-echo "--------------------------------------------";}}elseecho "
-Invalid user id or password";?
->
-</div>
-</div>
-</div>
-</body>
-</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "meinedatenbank";
+
+// Erstelle Verbindung
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Überprüfe Verbindung
+if ($conn->connect_error) {
+    die("Verbindung fehlgeschlagen: " . $conn->connect_error);
+}
+
+$nachricht = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Benutzereingaben
+    $benutzername = $_POST['benutzername'];
+    $passwort = $_POST['passwort'];
+
+    // Unsichere SQL-Abfrage
+    $sql = "SELECT * FROM benutzer WHERE benutzername = '$benutzername' AND passwort = '$passwort'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $nachricht = "Benutzer gefunden!";
+    } else {
+        $nachricht = "Keine Benutzer gefunden.";
+    }
+}
+
+// Schließe die Verbindung
+$conn->close();
+
+// Inkludiere die HTML-Datei
+include 'form.html';
+?>
